80da599d506671214a930c41bd0b101b317bdac6
israelchidera <israelchidera54@gmail.com>
2020-07-19 01:52:03 +0100

    swiper js update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
 function openSlideMenu() {
         document.getElementById('side-menu').style.width = '250px';
         // document.getElementById('main').style.margin = '150px';
@@ -7,4 +16,3 @@ function closeSlideMenu() {
         document.getElementById('side-menu').style.width = '0';
         // document.getElementById('main').style.margin = 'auto';
     }
-
